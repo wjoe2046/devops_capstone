83249b67c4ea2f20a295049a6ebfe49ec4e6d575
Wenfei Zhou <wzhou2046@gmail.com>
2020-09-12 14:45:21 -0400

    debug

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile(__dirname + 'index.html');
 });
 
 app.listen(PORT, HOST);
